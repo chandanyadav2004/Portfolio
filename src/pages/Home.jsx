e22ad2b8bdab9e0df3504b5e30ec110c250183ea
chandanyadav2004 <demo8788151949@gmail.com>
2025-07-31 22:59:08 +0530

    Project completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+
+import ThemeToggle from '../components/ThemeToggle'
+import {StarBackground} from '../components/StarBackground'
+import { Navbar } from '../components/Navbar'
+import { HeroSection } from '../components/HeroSection'
+import { AboutMe } from '../components/AboutMe'
+import { SkillsSection } from '../components/SkillsSection'
+import { ProjectSection } from '../components/ProjectSection'
+import { ContactSection } from '../components/ContactSection'
+import { Footer } from '../components/Footer'
+
+export const Home = () => {
+  return (
+    <>
+        <div className='min-h-screen  bg-background text-foreground overflow-x-hidden'>
+            {/*  THEME Toggle   */}
+            <ThemeToggle />
+
+            {/* Background Effects */}
+
+            <StarBackground />
+
+
+            {/* Navbar */}
+
+            <Navbar />
+
+            {/* Main Content */}
+            <main>
+              <HeroSection />
+              <AboutMe />
+              <SkillsSection />
+              <ProjectSection />
+              <ContactSection />
+            </main>
+
+            {/* Footer  */}
+
+            <Footer />
+
+        </div>
+    
+    </>
+  )
+}
